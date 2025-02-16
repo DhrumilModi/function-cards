d00504f7ec5d3349f6bfa9b8e58965820514e613
Dhrumil Modi <dhrumil789@Dhrumils-MacBook-Pro.local>
2025-02-16 23:47:37 +0530

    type fixes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,13 +2,7 @@ import React from "react"
 import { CardProps } from "./Card.types"
 import CardRadio from "../CardRadio"
 
-const Card: React.FC<CardProps> = ({
-  title,
-  children,
-  id,
-  setLines,
-  target
-}) => {
+const Card: React.FC<CardProps> = ({ title, children, id, target }) => {
   return (
     <div className=" bg-white rounded-[15px] overflow-hidden border border-[#DFDFDF] shadow-[0px_0px_6px_0px_rgba(0,0,0,0.05)]">
       <div className="py-[13px] px-[20px] min-h-[251px] min-w-[235px] flex flex-col justify-between">
@@ -26,7 +20,7 @@ const Card: React.FC<CardProps> = ({
 
         <div className="flex justify-between mb-1 mt-auto">
           <div className="flex">
-            <CardRadio id={id} type="IN" setLines={setLines} target={target} />
+            <CardRadio id={id} type="IN" target={target} />
             <span className="text-[10px] ml-1 font-medium text-[#585757]">
               input
             </span>
@@ -35,7 +29,7 @@ const Card: React.FC<CardProps> = ({
             <span className="text-[10px] mr-1 font-medium text-[#585757]">
               output
             </span>
-            <CardRadio id={id} type="OUT" setLines={setLines} target={target} />
+            <CardRadio id={id} type="OUT" target={target} />
           </div>
         </div>
       </div>
